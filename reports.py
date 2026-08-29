06c6dd7815adb82a8d09e35445262688b5c95d81
piyushhnaikk <piyushhnaikk@gmail.com>
2026-08-29 20:06:38 +0100

    feat: add Excel report formatting

--- reports.py
+++ reports.py
@@ -1,5 +1,6 @@
 import pandas as pd 
 import openpyxl as xl 
+from openpyxl.styles import Font
 
 def get_reports(filename : str, df_valid, df_invalid,df_summary):
     path = "output/" + filename
@@ -8,6 +9,19 @@ def get_reports(filename : str, df_valid, df_invalid,df_summary):
         df_valid.to_excel(writer, sheet_name = "Cleaned Data", index = False)
         df_invalid.to_excel(writer, sheet_name = "Flagged Data", index = False)
         df_summary.to_excel(writer, sheet_name = "Summary", index = False )
+    
+    workbook = xl.load_workbook(path)
+
+    valid_sheet = workbook["Cleaned Data"]
+    invalid_sheet = workbook["Flagged Data"]
+    summary_sheet = workbook["Summary"]
+    
+    style_reports(valid_sheet)
+    style_reports(invalid_sheet)
+    style_reports(summary_sheet)
+
+    workbook.save(path)
+    
 
 def df_summary(df, df_valid, df_invalid):
     summary = {
@@ -31,3 +45,14 @@ def df_summary(df, df_valid, df_invalid):
     df_summary = pd.DataFrame(summary)
 
     return df_summary
+
+def style_reports(sheet):
+    for cell in sheet[1]:
+        cell.font = Font(bold = True)
+
+    for col in sheet.columns:
+        largest = 0
+        for cell in col:
+            if cell.value is not None and len(str(cell.value)) > largest:
+                largest = len(str(cell.value))
+        sheet.column_dimensions[col[0].column_letter].width = largest + 5
